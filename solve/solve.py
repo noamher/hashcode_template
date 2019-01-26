554a88a4a99fee7010bf291423c7399b88c75f23
Noam Hershtig <noam@beeeye.co>
2019-01-26 11:40:42 +0200

    Add result checker support

--- solve/solve.py
+++ solve/solve.py
@@ -16,25 +16,41 @@ def actual_solve(inp):
     return res
 
 
-def solve(input_file, persist_dir=None):
+def solve(input_file, persist_dir=None, checker=False):
     print("Parsing input file '{}'".format(input_file))
     inp = parse_input(input_file)
 
     res = actual_solve(inp)
 
+    score = None
+    if checker:
+        score = check(res)
+        print("Score for input file '{}' is {}".format(input_file, score))
+
     output_file = Path(input_file).with_suffix('.out')
     print("Writing output to file '{}'".format(output_file))
-    write_output(output_file, res, persist_dir)
+    write_output(output_file, res, persist_dir, score)
 
 
-def write_output(output_file, result, persist_dir):
+def check(result):
+    # <insert score checking code...>
+    assert 1 < 2
+    return 42
+
+
+def write_output(output_file, result, persist_dir, score):
     with output_file.open('w') as f:
         # <insert code to write result to file ...>
         for l in result:
             f.write('{}\n'.format(l).decode('utf-8'))
 
     if persist_dir:
-        shutil.copy(str(output_file), str(persist_dir))
+        persist_output_file = persist_dir / '{}{}{}'.format(
+            output_file.stem,
+            '__{}'.format(score) if score else '',
+            output_file.suffix,
+        )
+        shutil.copy(str(output_file), str(persist_output_file))
 
 
 def parse_input(input_file):
@@ -61,8 +77,7 @@ def main(args):
         print("Copying solution package to persist dir")
         shutil.copytree(str(solution_dir), str(persist_dir))
 
-
-    jobs = [delayed(solve)(input_file, persist_dir) for input_file in args.input_files]
+    jobs = [delayed(solve)(input_file, persist_dir, args.checker) for input_file in args.input_files]
     cpus = -1 if args.parallel else 1
     Parallel(n_jobs=cpus)(jobs)
 
@@ -71,6 +86,7 @@ def get_parser():
     parser = argparse.ArgumentParser(description='Hash Code solver')
     parser.add_argument('input_files', type=str, nargs='+')
     parser.add_argument('--parallel', action='store_true')
+    parser.add_argument('--checker', action='store_true')
     parser.add_argument('--persist', type=str, default=None)
     return parser
 
