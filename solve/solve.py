2d38df71ec715c4a15923521e6a2d65f43a37733
Noam Hershtig <noam@beeeye.co>
2019-02-10 20:46:02 +0200

    Outputs always go to dedicated output directory

--- solve/solve.py
+++ solve/solve.py
@@ -16,7 +16,7 @@ def actual_solve(inp):
     return res
 
 
-def solve(input_file, persist_dir=None, checker=False):
+def solve(input_file, output_dir, checker=False):
     print("Parsing input file '{}'".format(input_file))
     inp = parse_input(input_file)
 
@@ -27,9 +27,7 @@ def solve(input_file, persist_dir=None, checker=False):
         score = check(res)
         print("Score for input file '{}' is {}".format(input_file, score))
 
-    output_file = Path(input_file).with_suffix('.out')
-    print("Writing output to file '{}'".format(output_file))
-    write_output(output_file, res, persist_dir, score)
+    write_output(output_dir, input_file, res, score)
 
 
 def check(result):
@@ -38,19 +36,18 @@ def check(result):
     return 42
 
 
-def write_output(output_file, result, persist_dir, score):
-    with output_file.open('w') as f:
+def write_output(output_dir, input_file, result, score):
+    output_file_name = output_dir / Path(input_file).with_suffix('.out').name
+    if score is not None:
+        file_name = '{}__{}.out'.format(Path(input_file).stem, score)
+        output_file_name = output_dir / Path(input_file).with_name(file_name)
+
+    print("Writing output to file '{}'".format(output_file_name))
+
+    with Path(output_file_name).open('w') as f:
         # <insert code to write result to file ...>
         for l in result:
             f.write('{}\n'.format(l).decode('utf-8'))
-
-    if persist_dir:
-        persist_output_file = persist_dir / '{}{}{}'.format(
-            output_file.stem,
-            '__{}'.format(score) if score else '',
-            output_file.suffix,
-        )
-        shutil.copy(str(output_file), str(persist_output_file))
 
 
 def parse_input(input_file):
@@ -66,46 +63,34 @@ def parse_input(input_file):
     return inp
 
 
-def persist_last(persist_alias):
-    print("Persisting last results and current code")
-
-    solution_dir, persist_dir = _persist_dirs(persist_alias)
-    output_dir = solution_dir.parent
+def persist(persist_alias, output_dir):
+    persist_dir = _persist_dir(persist_alias)
 
     print("Copying solution package to persist dir")
-    shutil.copytree(str(solution_dir), str(persist_dir))
-
-    print("Copying last outputs to persist dir")
-    for src in output_dir.glob('*.out'):
-        shutil.copy(str(src), str(persist_dir))
-
-
-def persist(persist_alias):
-    solution_dir, persist_dir = _persist_dirs(persist_alias)
-
-    print("Copying solution package to persist dir")
-    shutil.copytree(str(solution_dir), str(persist_dir))
+    shutil.copytree(str(output_dir), str(persist_dir))
     return persist_dir
 
 
-def _persist_dirs(alias):
-    solution_dir = Path(__file__).parent
-    persist_base_dir = solution_dir.parent / 'persist'
+def _persist_dir(alias):
+    persist_base_dir = Path(__file__).parent.parent / 'persist'
     persist_base_dir.mkdir(parents=True, exist_ok=True)
     persist_dir = persist_base_dir / '{}_{}'.format(datetime.now().strftime('%H%M%S'), alias)
-    return solution_dir, persist_dir
+    return persist_dir
 
 
 def main(args):
-    if args.persist_last:
-        persist_last(args.persist_last)
+    output_dir = Path(__file__).parent.parent / 'output'
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    if args.persist:
+        persist(args.persist, output_dir)
         return
 
-    persist_dir = None
-    if args.persist:
-        persist_dir = persist(args.persist)
+    # erase previous results
+    for f in output_dir.glob('./*.out'):
+        f.unlink()
 
-    jobs = [delayed(solve)(input_file, persist_dir, args.checker) for input_file in args.input_files]
+    jobs = [delayed(solve)(input_file, output_dir, args.checker) for input_file in args.input_files]
     cpus = -1 if args.parallel else 1
     Parallel(n_jobs=cpus)(jobs)
 
@@ -115,8 +100,7 @@ def get_parser():
     parser.add_argument('input_files', type=str, nargs='+')
     parser.add_argument('--parallel', action='store_true')
     parser.add_argument('--checker', action='store_true')
-    parser.add_argument('--persist', type=str, default=None)
-    parser.add_argument('--persist-last', type=str, default=None,
+    parser.add_argument('--persist', type=str, default=None,
                         help="Persist current code and outputs of last run and then exit.")
     return parser
 
